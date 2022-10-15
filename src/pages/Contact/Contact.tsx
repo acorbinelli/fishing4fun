d4a11145640bed16ec716fd98674bad154da0b83
Alessandro Corbinelli <acorbinelli@gmail.com>
2022-10-15 20:02:33 +0300

    create contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,10 +1,92 @@
-import { Box } from "@mui/material";
+import { Box, TextField, Button, Typography, IconButton } from "@mui/material";
+import { useCallback } from "react";
 import IntroText from "shared/components/IntroText";
 import UnderConstruction from "components/UnderConstruction";
+import useMobileDetect from "hooks/useMobileDetect";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Contact = () => {
-  return <UnderConstruction />;
+  const { isSmall } = useMobileDetect();
 
+  const onYoutubeClick = useCallback(() => {
+    window.location.href = "https://www.youtube.com/channel/UCAuUBbW9p22VoDlZutn4JYg";
+  }, []);
+  const onFacebookClick = useCallback(() => {
+    window.location.href = "https://www.facebook.com/profile.php?id=100085277480233";
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 30,
+        position: "relative",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          p: 2,
+          background: "none",
+          width: (theme) => theme.spacing(isSmall ? 40 : 100),
+        }}
+      >
+        <Typography variant={isSmall ? "h4" : "h3"} sx={{ color: "primary.light" }}>
+          Suntem încântați de cunostință!
+        </Typography>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          p: 2,
+          background: "none",
+          width: (theme) => theme.spacing(isSmall ? 40 : 100),
+        }}
+      >
+        <TextField
+          size="small"
+          variant="outlined"
+          fullWidth
+          color="warning"
+          sx={{ backgroundColor: "primary.light", borderRadius: 2, mb: 2 }}
+          placeholder="Nume"
+        />
+        <TextField
+          variant="outlined"
+          fullWidth
+          color="warning"
+          sx={{ backgroundColor: "primary.light", borderRadius: 2, mb: 2 }}
+          placeholder="Mesaj"
+          multiline
+          minRows={6}
+        />
+        <Button variant="contained" color="warning">
+          Trimite
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          background: "none",
+          width: (theme) => theme.spacing(isSmall ? 40 : 100),
+        }}
+      >
+        <IconButton onClick={onYoutubeClick} size="large">
+          <YouTubeIcon fontSize="large" color="warning" />
+        </IconButton>
+        <IconButton onClick={onFacebookClick} size="large">
+          <FacebookIcon fontSize="large" color="warning" />
+        </IconButton>
+      </Box>
+    </Box>
+  );
 };
 
 export default Contact;
